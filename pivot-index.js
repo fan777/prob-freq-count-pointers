7f667d794b9c69e7d53dc3ece1db569c3f064ba6
fan777 <fan777@gmail.com>
2021-09-10 23:31:16 -0700

    complete pivot

--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function pivotIndex() {
+function pivotIndex(arr) {
+  if (arr.length < 2) return -1;
+
+  let totalSum = arr.reduce((prev, curr) => prev + curr);
+  let leftSum = 0;
+  let rightSum = 0;
+
+  for (let i = 2; i < arr.length - 1; i++) {
+    leftSum += arr[i - 2]
+    rightSum = totalSum - leftSum - arr[i - 1]
+    if (leftSum === rightSum)
+      return i - 1;
+  }
+  return -1;
 }
